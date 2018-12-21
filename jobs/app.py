d203c7dfad3fac968e6d779af97646082cc9ca4c
Ashok <ashokbalusu@hotmail.com>
2018-12-21 17:58:25 -0600

    Finished Flask Setup Module 2

---
+++ jobs/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+# create a Flast instance in the name of app passing a special variable name surrounded by double underscores
+app = Flask(__name__)
+
+# create a single route
+# in order for this jobs() function to act as a route, we add 2 route decorators 
+@app.route('/')
+@app.route('/jobs')
+def jobs():
+    return render_template('index.html')
